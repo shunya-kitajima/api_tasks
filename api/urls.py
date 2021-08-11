a1a2876915cff4fa9816830c6423b380b344b8bb
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-11 23:16:33 +0900

    add files

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
